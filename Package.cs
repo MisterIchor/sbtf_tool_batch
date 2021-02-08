d20ee9f1e213b35c529f77a802b26965e45ea1c2
ctrl-alt-rabbit <78709806+ctrl-alt-rabbit@users.noreply.github.com>
2021-02-08 02:01:34 +0100

    Initial commit

---
+++ Package.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Diagnostics;
+using System.Xml;
+
+namespace sbtftool
+{
+    [DataContract]
+    public class PackageFile
+    {
+        [DataMember] public string FilePath { get; private set; }
+        [DataMember] public short Unknown1 { get; private set; }
+        [DataMember] public short Unknown2 { get; private set; }
+        [DataMember] public int Unknown3 { get; private set; }
+
+        public int Size { get; set; }
+        public int ComputedOffset { get; set; }
+
+        public PackageFile(string filePath, short unknown1, short unknown2, int unknown3)
+        {
+            FilePath = filePath;
+            Unknown1 = unknown1;
+            Unknown2 = unknown2;
+            Unknown3 = unknown3;
+        }
+    }
+
+    [DataContract]
+    public class Package
+    {
+        [DataMember] public List<PackageFile> Files { get; private set; }
+
+        public static Package ReadPackageFromNwf(Stream file)
+        {
+            using BinaryReader reader = new BinaryReader(file, Encoding.ASCII, true);
+            var files = new List<PackageFile>();
+
+            try
+            {
+                int magic = reader.ReadInt32();
+                if (magic != 0x6E776660)
+                {
+                    throw new PackageParseException("Magic number is incorrect.");
+                }
+
+                // Unknown value
+                var _ = reader.ReadInt32();
+                var fileCount = reader.ReadInt32();
+                Trace.WriteLine($"File count: {fileCount}");
+
+                for (int i = 0; i < fileCount; i++)
+                {
+                    var fileNameLength = reader.ReadInt32();
+                    var fileNameBytes = reader.ReadBytes(fileNameLength);
+                    var fileName = Encoding.UTF8.GetString(fileNameBytes);
+                    Trace.WriteLine($"File name: {fileName}");
+
+                    var unknown1 = reader.ReadInt16();
+                    var unknown2 = reader.ReadInt16();
+                    var unknown3 = reader.ReadInt32();
+                    Trace.WriteLine($"\tUnknown values: {unknown1} {unknown2} {unknown3}");
+
+                    var offset = reader.ReadInt32();
+                    var size = reader.ReadInt32();
+                    Trace.WriteLine($"\tOffset: {offset}, size: {size}");
+
+                    files.Add(new PackageFile(fileName, unknown1, unknown2, unknown3)
+                    {
+                        ComputedOffset = offset,
+                        Size = size
+                    });
+                }
+
+                Trace.WriteLine("Done reading package file");
+
+                return new Package { Files = files };
+            }
+            catch (IOException io)
+            {
+                throw new PackageParseException("Error while parsing package.", io);
+            }
+        }
+
+        public static Package ReadPackageDefinitionFromXml(Stream input)
+        {
+            var ser = new DataContractSerializer(typeof(Package));
+            return (Package)ser.ReadObject(input);
+        }
+
+        public void WritePackageDefinitionToXml(Stream output)
+        {
+            var ser = new DataContractSerializer(GetType());
+            using var @out = XmlWriter.Create(output, new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true,
+                CloseOutput = false
+            });
+
+            ser.WriteObject(@out, this);
+        }
+
+        public class PackageParseException : Exception
+        {
+            public PackageParseException()
+            {
+            }
+
+            public PackageParseException(string message)
+                : base(message)
+            {
+            }
+
+            public PackageParseException(string message, Exception inner)
+                : base(message, inner)
+            {
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using CommandLine;
+
+namespace sbtftool
+{
+    class Program
+    {
+        [Verb("unpack", HelpText = "Unpack a .nwf file")]
+        class UnpackOptions
+        {
+            [Value(0, Required = true, HelpText = ".nwf file")]
+            public string NwfFile { get; set; }
+
+            [Option('o', "output", Default = "output", HelpText = "Directory to unpack files into (default: output)")]
+            public string Output { get; set; }
+        }
+
+        [Verb("schema", HelpText = "Save the structure of a .nwf file as XML file (for use in repacking)")]
+        class GenerateSchemaOptions
+        {
+            [Value(0, Required = true, HelpText = ".nwf file to generate schema from")]
+            public string NwfFile { get; set; }
+
+            [Value(1, Default = "schema.xml", HelpText = "Output file location")]
+            public string SchemaFile { get; set; }
+        }
+
+        [Verb("repack", HelpText = "Pack files back into .nwf")]
+        class RepackOptions
+        {
+            [Value(0, Required = true, HelpText = "XML Schema file to use for the packing")]
+            public string SchemaFile { get; set; }
+
+            [Value(1, Default = "output", HelpText = "Source folder containing all the assets to put into the .nwf file")]
+            public string SourceFolder { get; set; }
+
+            [Value(2, Default = "sbtf_pub.nwf", HelpText = "Path of the output file")]
+            public string NwfFile { get; set; }
+        }
+
+        static int Main(string[] args)
+        {
+            // return RealMain(new string[] { "unpack", @"C:\Program Files (x86)\Steam\steamapps\common\Space Beast Terror Fright\sbtf_pub.nwf" });
+
+            // return RealMain(new string[] { "schema", @"C:\Program Files (x86)\Steam\steamapps\common\Space Beast Terror Fright\sbtf_pub.nwf" });
+
+            // return RealMain(new string[] { "repack", "schema.xml", "output", "sbtf_pub.nwf" });
+
+            return RealMain(args);
+        }
+
+        static int RealMain(string[] args)
+        {
+            return CommandLine.Parser.Default.ParseArguments<UnpackOptions, GenerateSchemaOptions, RepackOptions>(args)
+                .MapResult(
+                    (UnpackOptions opts) => Unpack(opts),
+                    (GenerateSchemaOptions opts) => GenerateSchema(opts),
+                    (RepackOptions opts) => Repack(opts),
+                    (errs) => 1
+                );
+        }
+
+        private static int Unpack(UnpackOptions opts)
+        {
+            Directory.CreateDirectory(opts.Output);
+
+            using var nwfFile = File.OpenRead(opts.NwfFile);
+            var package = Package.ReadPackageFromNwf(nwfFile);
+            nwfFile.Seek(0, SeekOrigin.Begin);
+            var fileCount = package.Files.Count;
+
+            try
+            {
+                foreach (var (file, index) in package.Files.Select((f, i) => (f, i)))
+                {
+                    Console.WriteLine($"Reading file {file.FilePath}... ({index + 1}/{fileCount})");
+
+                    if (file.Size > 52428800)
+                    {
+                        throw new ArgumentException("File size sanity check failed (size > 50 MiB)");
+                    }
+
+                    var buf = new byte[file.Size];
+
+                    nwfFile.Seek(file.ComputedOffset, SeekOrigin.Begin);
+                    nwfFile.Read(buf, 0, file.Size);
+
+                    var outPath = Path.Join(opts.Output, file.FilePath.Replace('/', Path.DirectorySeparatorChar));
+                    Console.WriteLine($"Writing file to {outPath}");
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(outPath));
+                    using var outFile = File.Create(outPath);
+                    outFile.Write(buf, 0, file.Size);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Failed to unpack file: {e.Message}\n{e}");
+                return 1;
+            }
+
+            Console.WriteLine("Done!");
+            return 0;
+        }
+
+        private static int GenerateSchema(GenerateSchemaOptions opts)
+        {
+            using var nwfFile = File.OpenRead(opts.NwfFile);
+            var package = Package.ReadPackageFromNwf(nwfFile);
+            using var outFile = File.Create(opts.SchemaFile);
+            package.WritePackageDefinitionToXml(outFile);
+
+            return 0;
+        }
+
+        private static int Repack(RepackOptions opts)
+        {
+            using var schemaFile = File.OpenRead(opts.SchemaFile);
+            var package = Package.ReadPackageDefinitionFromXml(schemaFile);
+
+            // Make sure that all the files we want actually exist
+            // Also figure out how long the header is
+            // 12 bytes for the header, 20 + fileNameLength bytes for each file entry
+            var currentOffset = 12;
+
+            foreach (var file in package.Files)
+            {
+                var inPath = Path.Join(opts.SourceFolder, file.FilePath);
+                if (!File.Exists(inPath))
+                {
+                    Console.WriteLine($"Repacking failed: The schema wants file \"{file.FilePath}\", " +
+                        $"but I can't find it at {Path.GetFullPath(inPath)}");
+                    return 1;
+                }
+
+                currentOffset += 20 + file.FilePath.Length;
+            }
+
+            using var outFile = File.Create(opts.NwfFile);
+            using var writer = new BinaryWriter(outFile, Encoding.ASCII, true);
+
+            // Magic number, unknown, file count
+            writer.Write(0x6E776660);
+            writer.Write(0);
+            writer.Write(package.Files.Count);
+
+            foreach (var file in package.Files)
+            {
+                var inPath = Path.Join(opts.SourceFolder, file.FilePath);
+                using var inFile = File.OpenRead(inPath);
+                writer.Write(file.FilePath.Length);
+                writer.Write(Encoding.ASCII.GetBytes(file.FilePath));
+                writer.Write(file.Unknown1);
+                writer.Write(file.Unknown2);
+                writer.Write(file.Unknown3);
+                writer.Write(currentOffset);
+
+                // Length is a long in C# but the format uses ints
+                writer.Write((int)inFile.Length);
+                currentOffset += (int)inFile.Length;
+            }
+
+            writer.Close();
+
+            foreach (var file in package.Files)
+            {
+                var inPath = Path.Join(opts.SourceFolder, file.FilePath);
+                using var inFile = File.OpenRead(inPath);
+                inFile.CopyTo(outFile);
+            }
+
+            return 0;
+        }
+    }
+}
